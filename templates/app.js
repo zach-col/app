afb0b2f7637d617708476e00eb9c1978e7378bd3
Sundew Shin <sundew@wednus.com>
2018-11-07 15:26:13 -0600

    added status checking

--- templates/app.js
+++ templates/app.js
@@ -13,33 +13,11 @@ var recordButton = document.getElementById("recordButton");
 var stopButton = document.getElementById("stopButton");
 var pauseButton = document.getElementById("pauseButton"); 
  
-//add events to those 3 buttons
 recordButton.addEventListener("click", startRecording);
-//stopButton.addEventListener("click", stopRecording);
-//pauseButton.addEventListener("click", pauseRecording);
 
 function startRecording() {
     console.log("recordButton clicked");
- 
-    /*
-    Simple constraints object, for more advanced audio features see
-    <div class="video-container"><blockquote class="wp-embedded-content" data-secret="vNsz0nPBL4"><a href="https://addpipe.com/blog/audio-constraints-getusermedia/">Supported Audio Constraints in getUserMedia()</a></blockquote><iframe class="wp-embedded-content" sandbox="allow-scripts" security="restricted" style="position: absolute; clip: rect(1px, 1px, 1px, 1px);" src="https://addpipe.com/blog/audio-constraints-getusermedia/embed/#?secret=vNsz0nPBL4" data-secret="vNsz0nPBL4" width="600" height="338" title="“Supported Audio Constraints in getUserMedia()” — Pipe Blog" frameborder="0" marginwidth="0" marginheight="0" scrolling="no"></iframe></div>
-    */
- 
     var constraints = { audio: true, video:false }
- 
-    /*
-    Disable the record button until we get a success or fail from getUserMedia()
-    */
- 
-    //recordButton.disabled = true;
-    //stopButton.disabled = false;
-    //pauseButton.disabled = false
- 
-    /*
-    We're using the standard promise based getUserMedia()
-    https://developer.mozilla.org/en-US/docs/Web/API/MediaDevices/getUserMedia
-    */
  
     navigator.mediaDevices.getUserMedia(constraints).then(function(stream) {
         console.log("getUserMedia() success, stream created, initializing Recorder.js ...");
@@ -76,11 +54,11 @@ function startRecording() {
                   $('#divLoading').hide();
                   stopRecording();
                   $progressBar.css('width', '0%');  // init. back
-                }, 2000); // WAIT 3 milliseconds
-              }, 2000); // WAIT 3 milliseconds
-            }, 2000); // WAIT 3 seconds
-          }, 2000); // WAIT 3 seconds
-        }, 2000); // WAIT 1 second
+                }, 2000); // WAIT 2 milliseconds
+              }, 2000); // WAIT 2 milliseconds
+            }, 2000); // WAIT 2 seconds
+          }, 2000); // WAIT 2 seconds
+        }, 2000); // WAIT 2 second
 
 
 
@@ -108,14 +86,6 @@ function pauseRecording(){
 function stopRecording() {
     console.log("stopButton clicked");
  
-    //disable the stop button, enable the record too allow for new recordings
-    //stopButton.disabled = true;
-    //recordButton.disabled = false;
-    //pauseButton.disabled = true;
- 
-    //reset button just in case the recording is stopped while paused
-    //pauseButton.innerHTML="Pause";
- 
     //tell the recorder to stop the recording
     rec.stop();
  
@@ -127,7 +97,6 @@ function stopRecording() {
 }
 
 function createDownloadLink(blob) {
- 
     var url = URL.createObjectURL(blob);
     var au = document.createElement('audio');
     var li = document.createElement('li');
@@ -146,13 +115,13 @@ function createDownloadLink(blob) {
     //add the new audio and a elements to the li element
     li.appendChild(au);
     li.appendChild(link);
-    
+
     var filename = new Date().toISOString(); //filename to send to server without extension
-//upload link
-var upload = document.createElement('a');
-upload.href="#";
-upload.innerHTML = "Upload";
-upload.addEventListener("click", function(event){
+    //upload link
+    var upload = document.createElement('a');
+    upload.href="#";
+    upload.innerHTML = "Upload";
+    upload.addEventListener("click", function(event){
       var xhr=new XMLHttpRequest();
       xhr.onload=function(e) {
           if(this.readyState === 4) {
@@ -163,9 +132,9 @@ upload.addEventListener("click", function(event){
       fd.append("audio_data",blob, filename);
       xhr.open("POST","upload.php",true);
       xhr.send(fd);
-})
-li.appendChild(document.createTextNode (" "))//add a space in between
-li.appendChild(upload)//add the upload link to li
+    })
+    li.appendChild(document.createTextNode (" "))//add a space in between
+    li.appendChild(upload)//add the upload link to li
 
     //add the li element to the ordered list
     recordingsList.appendChild(li);
--- testApiCall.py
+++ testApiCall.py
@@ -1,24 +1,25 @@
 import requests
 import json
-
+import time
 
 headers = {
-    'Authorization': 'Bearer put_api_key_here',
+    'Authorization': 'Bearer 01M_lVdLpTBXgCcW3PtIAXASKjrS7wx4tUrLCV2WXW10DvjzP16l0dnRLziUTMbc1hL5qBJvhePqCZHZiRveKx07DWJ2w',
     'Content-Type': 'application/json',
 }
 # submit audio file
 data = '{"media_url":"http://www.wavsource.com/snds_2018-06-03_5106726768923853/people/famous/king_injustice.wav","metadata":"This is a sample submit jobs option"}'
 response = requests.post('https://api.rev.ai/revspeech/v1beta/jobs', headers=headers, data=data).json()
-print "got the id", response['id']
-
+print ("got the id", response['id'])
+jobId = response['id']
 
 headers = {
-    'Authorization': 'Bearer put_api_key_here',
+    'Authorization': 'Bearer 01M_lVdLpTBXgCcW3PtIAXASKjrS7wx4tUrLCV2WXW10DvjzP16l0dnRLziUTMbc1hL5qBJvhePqCZHZiRveKx07DWJ2w',
 }
-response = requests.get('https://api.rev.ai/revspeech/v1beta/jobs/' + response['id'], headers=headers).json()
 
-print response
-
+while (response['status'] != 'transcribed'):
+    response = requests.get('https://api.rev.ai/revspeech/v1beta/jobs/'+ jobId, headers=headers).json()
+    print (response['status'])
+    time.sleep(0.1)
 
 # gets audio file text
 #newHeaders = {
