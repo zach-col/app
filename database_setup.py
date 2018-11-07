f42abc276cf655cdee5d9aeaeedfddf9eec6113b
zach-col <theperfectpicture100@gmail.com>
2018-11-07 12:43:41 -0600

    feat: add database

---
+++ database_setup.py
@@ -0,0 +1,20 @@
+import os
+import sys
+from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+from sqlalchemy import desc
+from sqlalchemy import create_engine
+
+Base = declarative_base()
+# days
+class Day(Base):
+    __tablename__ = 'day'
+
+    id = Column(Integer, primary_key=True)
+    date = Column(String(250), nullable=False)
+    href = Column(String(250), nullable=False)
+    text = Column(String(250), nullable=False)
+
+engine = create_engine('sqlite:///days.db')
+Base.metadata.create_all(engine)
