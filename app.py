61e7087721013e08325d0ca4ee25e9bc6e75a614
zach-col <theperfectpicture100@gmail.com>
2018-11-07 19:00:01 -0600

    feat: add query to database

--- app.py
+++ app.py
@@ -1,38 +1,35 @@
 from flask import Flask
-from flask import render_template
+from flask import render_template, url_for, redirect, request
+
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return render_template('index.html')
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from database_setup import Base,Day
 
-@app.route('/login')
-def login():
-    return render_template('login.html')
+engine = create_engine('sqlite:///days.db')
+Base.metadata.bind = engine
+DBSession = sessionmaker(bind=engine)
+session = DBSession()
 
-@app.route('/home')
-def home():
-    return render_template('home.html')
+@app.route('/', methods=['GET','POST'])
+def newday():
+    daycount = session.query(Day).count()
+    return render_template('newfile.html', daycount = daycount)
 
-@app.route('/voiceupload/')
-def voiceupload():
-    # upload api and upload
+@app.route('/file/<file_name>', methods=['GET','POST'])
+def filename(file_name):
 
-    # send text saying its uploaded
-    # redirect to state
-    return render_template('voiceupload.html')
+    # create database query
+    file = Day(date="111111", href="linklink",
+                text='text here')
+    session.add(file)
+    session.commit()
+    daycount = session.query(Day).count()
+    return redirect(url_for('newday'))
 
 
-@app.route('/voiceupload/state/')
-def voiceuploadstate():
-    # get upload state
-    # if state succes save
-    # then redirect to all uploads
-    return render_template('voiceuploadstate.html')
-
-@app.route('/alluploads')
-def allvoiceupload():
-    return render_template('alluploads.html')
 
 if __name__ == '__main__':
+    app.debug = True
     app.run(host='0.0.0.0', port=5000)
