d09ae60c5ea01755c7e2da92208b3db9d7288a21
Sundew Shin <sundew@wednus.com>
2018-11-07 18:28:03 -0600

    commit Ellie's changes

---
+++ templates/pageflow.js
@@ -0,0 +1,31 @@
+$('.navbar .home').click(function(){
+    $('.container').hide();
+    $('.home-container').show();
+
+    $('.nav-item').removeClass('active');
+    $('.nav-item.home').addClass('active');
+});
+
+$('.navbar .help').click(function(){
+    $('.container').hide();
+    $('.help-container').show();
+
+    $('.nav-item').removeClass('active');
+    $('.nav-item.help').addClass('active');
+});
+
+$('.navbar .record').click(function(){
+    $('.container').hide();
+    $('.record-container').show();
+
+    $('.nav-item').removeClass('active');
+    $('.nav-item.record').addClass('active');
+});
+
+$('.navbar .transcripts').click(function(){
+    $('.container').hide();
+    $('.transcript-table-container').show();
+
+    $('.nav-item').removeClass('active');
+    $('.nav-item.transcripts').addClass('active');
+});
